3d5935074fd4374343148282a5d69e2bc633d3ad
aapelix <115158315+aapelix@users.noreply.github.com>
2023-06-14 10:25:35 +0300

    .

--- index.js
+++ index.js
@@ -279,20 +279,4 @@ async function runCmd(command, print) {
 	})
 }
 
-/* Not Cli Stuff */
 
-function runCommand(command) {
-	exec(command, (error, stdout, stderr) => {
-		if (error) {
-			console.log(`Error exexcuting command: ${error.message}`);
-		}
-		if (stderr) {
-	  		console.error(stderr);
-	  		return;
-		}
-		
-		console.log(stdout);
-	})
-}
-
-module.exports.runCommand = runCommand;
